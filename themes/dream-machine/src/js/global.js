5e5c3043dee9386132bb4a2635814bf58406babf
Vladimir Gutorov <vlgutv@gmail.com>
2017-10-28 01:51:26 +0300

    Add animated background

--- themes/dream-machine/src/js/global.js
+++ themes/dream-machine/src/js/global.js
@@ -1,1 +1,15 @@
 import "../sass/global.sass";
+import slick from "slick-carousel";
+
+let jQuery = require("jquery");
+
+jQuery(document).ready(function() {
+  jQuery('.mainPageHeader__slider').slick({
+    arrows: false,
+    fade: true,
+    autoplay: true,
+    speed: 1000,
+    pauseOnHover: false,
+    autoplaySpeed: 5000
+  });
+});
